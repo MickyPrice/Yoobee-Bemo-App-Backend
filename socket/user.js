e7d42fa436d2edd335898ca323b33fe16f67a93e
Michael Price <realmprice@gmail.com>
2020-10-07 22:21:16 +1300

    :lock: Remove all sensitive and useless data from userSearchResponse event emit

--- socket/user.js
+++ socket/user.js
@@ -4,11 +4,26 @@ const User = require("../models/User.js");
 const mongoose = require("mongoose");
 const socket = require("./index.js");
 
+/**
+ * 
+ * @param {Object} io - SocketIo object
+ * @param {Object} socket - Socket Object
+ * @param {String} query - The query
+ */
 const getUsersBeginWith = (io, socket, query) => {
     query = query.replace(/\W/g, '');
     var regexp = new RegExp("^" + query, "i");
     User.find({ $or: [{ username: regexp }, { fullname: regexp }] }).then((data) => {
-        socket.emit("userSearchResponse", data);
+        let users = [];
+        for (let i = 0; i < data.length; i++) {
+            const dbuser = data[i];
+            users.push({
+                _id: dbuser._id,
+                username: dbuser.username,
+                fullname: dbuser.fullname
+            });
+        }
+        socket.emit("userSearchResponse", users);
     }).catch((err) => {
         console.log("ERR", err);
     });
