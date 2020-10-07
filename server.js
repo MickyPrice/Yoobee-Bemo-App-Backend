b1f8e0937cdcf6a1e3a71e0402a033190b3f390a
ogriffithjones <ogriffithjones@gmail.com>
2020-10-08 11:25:40 +1300

    Add console logs for checking

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.io = io;
 const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
 require("./socket")(io);
-
+console.log("Required Io");
 
 // App Setup  ---------------------------------------------
 
@@ -71,9 +71,12 @@ io.use(wrap(initpassportSession));
 // Socket -------------------------------------------------
 
 io.use((socket, next) => {
+  console.log("Try Auth");
   if (socket.request.user) {
     next();
+    console.log("Auth Passed");
   } else {
+    console.log("Auth Failed");
     next(new Error('unauthorized'));
   }
 });
