290aadda4fb66678090c4a1b24d2b896c8b6b358
Michael Price <realmprice@gmail.com>
2020-10-11 23:26:48 +1300

    Add feature to give users a 00 balance to begin with for the sake of demonstration

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
   verified: Boolean,
   picture: String,
   balance: {
-    type: Number, default: 0
+    type: Number, default: 50000
   },
   auth: String,
   accounts: Object,
