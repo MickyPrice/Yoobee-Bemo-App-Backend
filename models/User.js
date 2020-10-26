7caa82eb0ac3822265756d198d7b1e977d36bffa
ogriffithjones <ogriffithjones@gmail.com>
2020-10-26 20:00:35 +1300

    :bug: Bug fixes

--- models/User.js
+++ models/User.js
@@ -1,26 +1,30 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  fullname: { type: String, required: true },
-  userToken: Number,
-  pinCode: String,
-  verified: Boolean,
-  picture: String,
-  balance: {
-    type: Number, default: 0
+const UserSchema = new mongoose.Schema(
+  {
+    online: { type: Boolean, default: false },
+    username: { type: String, required: true },
+    fullname: { type: String, required: true },
+    userToken: Number,
+    pinCode: String,
+    verified: Boolean,
+    picture: String,
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    auth: String,
+    accounts: Object,
+    channels: [{ type: mongoose.Types.ObjectId, ref: "Channel" }],
+    locked: {
+      type: Boolean,
+      default: false,
+    },
+    payments: [mongoose.Types.ObjectId],
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
   },
-  auth: String,
-  accounts: Object,
-  channels: [{ type: mongoose.Types.ObjectId, ref: 'Channel' }],
-  locked: {
-    type: Boolean,
-    default: false
-},
-  payments: [mongoose.Types.ObjectId],
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-},
-{ timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
